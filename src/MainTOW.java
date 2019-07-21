564d1c87d27ce584fa554ffb2dfb121ec827258d
lx <lx.@qq.com>
2019-07-21 18:04:52 +0800

    增加了一些类

---
+++ src/MainTOW.java
@@ -0,0 +1,5 @@
+public class MainTOW {
+    public static void main(String[] args) {
+        System.out.println("Hello MainTow!");
+    }
+}
---
+++ src/PassWord.java
@@ -0,0 +1,5 @@
+public class PassWord {
+    private  String password;
+
+
+}
